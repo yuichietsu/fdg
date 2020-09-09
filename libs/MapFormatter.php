00c76ecabdc923719836d9e47a3adcebe9e460d7
yuichietsu <mics.etsu@gmail.com>
2020-09-09 23:54:45 +0900

    RPGのパップデータとして使うためのデータとして吐き出すメソッドrpg()を追加

--- libs/MapFormatter.php
+++ libs/MapFormatter.php
@@ -8,7 +8,7 @@ class MapFormatter
     public $path = ' ';
     public $wall = '#';
 
-    public function format($map)
+    public function simple($map)
     {
         $text = '';
         $size = $map->getMatrixSize();
@@ -18,6 +18,39 @@ class MapFormatter
                 $text .= $x == 0 ? $value : "{$this->delimiter}{$value}";
             }
             $text .= "\n";
+        }
+        return $text;
+    }
+
+    public function rpg($map)
+    {
+        /*
+        Wall
+          1: U
+          2: R
+          4: D
+          8: L
+        Door
+         16: U
+         32: R
+         64: D
+        128: L
+         */
+        $text = '';
+        $size = $map->getMatrixSize();
+        for ($y = 1; $y < $size - 1; $y++) {
+            $strip = [];
+            for ($x = 1; $x < $size - 1; $x++) {
+                $v = 0;
+                if ($map->getMatrixValue($x, $y)) {
+                    !$map->getMatrixValue($x, $y - 1) && $v += 1;
+                    !$map->getMatrixValue($x + 1, $y) && $v += 2;
+                    !$map->getMatrixValue($x, $y + 1) && $v += 4;
+                    !$map->getMatrixValue($x - 1, $y) && $v += 8;
+                }
+                $strip[] = $v;
+            }
+            $text .= implode(',', $strip) . ",\n";
         }
         return $text;
     }
--- run.php
+++ run.php
@@ -5,7 +5,26 @@ error_reporting(E_ERROR);
 require_once "libs/MapMaker.php";
 require_once "libs/MapFormatter.php";
 
-$mm = new MapMaker();
-$mm->generate(20);
-$mf = new MapFormatter();
-echo $mf->format($mm);
+exit(main());
+
+function main()
+{
+    $r = 0;
+    $opts = getopt('t:');
+    $method = 'simple';
+    if (isset($opts['t'])) {
+        $method = $opts['t'];
+    }
+
+    $mm = new MapMaker();
+    $mm->generate(20);
+    $mf = new MapFormatter();
+    echo $mf->simple($mm);
+    if (method_exists($mf, $method)) {
+        echo $mf->$method($mm);
+    } else {
+        echo "method not found: $method\n";
+        $r = 1;
+    }
+    return $r;
+}
