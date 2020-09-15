e0ee5f0307f2dc39cda4ef6eede2d96fd20e01d8
yuichietsu <mics.etsu@gmail.com>
2020-09-15 23:31:39 +0900

    Android Resource形式の出力

--- libs/MapFormatter.php
+++ libs/MapFormatter.php
@@ -24,20 +24,6 @@ class MapFormatter
 
     public function rpg($map)
     {
-        /*
-        Wall
-          1: U
-          2: R
-          4: D
-          8: L
-        Door
-         16: U
-         32: R
-         64: D
-        128: L
-        Otehrs
-        256: In Walls
-         */
         $text = '';
         $size = $map->getMatrixSize();
         for ($y = 1; $y < $size - 1; $y++) {
@@ -57,4 +43,58 @@ class MapFormatter
         }
         return $text;
     }
+
+    public function androidResource($map)
+    {
+        $name = $this->args['opts']['name'] ?: 'map';
+        $size = $map->getMatrixSize();
+        $data = [];
+        for ($y = 1; $y < $size - 1; $y++) {
+            for ($x = 1; $x < $size - 1; $x++) {
+                $v = 256;
+                if ($map->getMatrixValue($x, $y)) {
+                    $v = 0;
+                    !$map->getMatrixValue($x, $y - 1) && $v += 1;
+                    !$map->getMatrixValue($x + 1, $y) && $v += 2;
+                    !$map->getMatrixValue($x, $y + 1) && $v += 4;
+                    !$map->getMatrixValue($x - 1, $y) && $v += 8;
+                }
+                $data[] = $v;
+            }
+        }
+        foreach ([512, 1024] as $s) {
+            $assigned = false;
+            while (!$assigned) {
+                for ($i = 0, $n = count($data); $i < $n; $i++) {
+                    if ($data[$i] === 0 && rand(0, $n) === $i) {
+                        $data[$i] = $s;
+                        $assigned = true;
+                        break;
+                    }
+                }
+            }
+        }
+        $xml = sprintf('<integer-array name="%s">', $name);
+        for ($i = 0, $n = count($data); $i < $n; $i++) {
+            $xml .= "<item>{$data[$i]}</item>";
+        }
+        $xml .= '</integer-array>';
+        return $xml;
+    }
 }
+/*
+Wall
+1: U
+2: R
+4: D
+8: L
+Door
+16: U
+32: R
+64: D
+128: L
+Otehrs
+256: In Walls
+512: UP
+1024: DOWN
+*/
--- run.php
+++ run.php
@@ -5,9 +5,9 @@ error_reporting(E_ERROR);
 require_once "libs/MapMaker.php";
 require_once "libs/MapFormatter.php";
 
-exit(main());
+exit(main(parseArgv($argv, 'v')));
 
-function main()
+function main($args)
 {
     $r = 0;
     $size = 20;
@@ -20,14 +20,48 @@ function main()
         $size = $opts['s'];
     }
     $mm = new MapMaker();
+    $mm->args = $args;
     $mm->generate($size);
     $mf = new MapFormatter();
+    $mf->args = $args;
     echo $mf->simple($mm);
     if (method_exists($mf, $method)) {
-        echo $mf->$method($mm);
+        echo $mf->$method($mm, $opts);
     } else {
         echo "method not found: $method\n";
         $r = 1;
     }
     return $r;
+}
+
+function parseArgv($argv, $flags = '')
+{
+    $args = [];
+    $opts = [];
+    $k = null;
+    for ($i = 1, $n = count($argv); $i < $n; $i++) {
+        $v = $argv[$i];
+        if (preg_match('/^--(\\S+)=(.+)$/', $v, $m)) {
+            if ($k !== null) {
+                $opts[$k] = null;
+            }
+            $k = null;
+            $opts[$m[1]] = $m[2];
+        } elseif (preg_match('/^-(\\S)$/', $v, $m)) {
+            if ($k !== null) {
+                $opts[$k] = null;
+            }
+            $k = $m[1];
+            if (strpos($flags, $k) !== false) {
+                $opts[$k] = null;
+                $k = null;
+            }
+        } elseif ($k !== null) {
+            $opts[$k] = $v;
+            $k = null;
+        } else {
+            $args[] = $v;
+        }
+    }
+    return ['args' => $args, 'opts' => $opts];
 }
